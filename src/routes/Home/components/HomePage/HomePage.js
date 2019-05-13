a044cd21e76d8013771d44fa1af806f3955d7296
Danielalab <olabolacrayola@hotmail.com>
2019-05-12 23:03:39 -0500

    add views basic

--- src/routes/Home/components/HomePage/HomePage.js
+++ src/routes/Home/components/HomePage/HomePage.js
@@ -8,22 +8,19 @@ import {
 } from 'constants/paths'
 
 import logo from '../../../../static/logo.png'
+// probando vistas
+import MentoringPage from 'routes/Mentoring/components/MentoringPage';
+import MenteePage from 'routes/Mentoring/components/menteePage/MenteePage';
+import MentorPage from 'routes/Mentoring/components/mentorPage/MentorPage';
+import FinancingPage from 'routes/Financing/FinancingPage';
 
 function Home() {
   return (
-    <section className="d-flex justify-content-center flex-column mx-4 vh-80">
-      <div className="d-flex justify-content-center flex-column">
-        <figure className="d-flex justify-content-center rounded-circle">
-          <img src={logo} className="img-fluid maxh-40vh rounded-circle" alt="fintee-logo"></img>
-        </figure>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis enim blanditiis
-          voluptatibus rem commodi dolorum maxime, modi adipisci! Iusto cum totam maxime?</p>
-      </div>
-      <div className="d-flex justify-content-center flex-column">
-        <button type="button" className="btn btn-primary btn-lg btn-block">Mentoring</button>
-        <button type="button" className="btn btn-primary btn-lg btn-block">Financing</button>
-      </div>
-    </section>
+
+    <FinancingPage />
+    // <MentorPage />
+    // <MenteePage />
+    // <MentoringPage />
   )
 }
 
