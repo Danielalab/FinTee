97dc181316cd1940011d3ea98e75a61961ea98ee
Danielalab <olabolacrayola@hotmail.com>
2019-05-15 16:41:42 -0500

    change home brand

--- src/routes/Home/components/HomePage/HomePage.js
+++ src/routes/Home/components/HomePage/HomePage.js
@@ -8,18 +8,13 @@ import {
 } from 'constants/paths'
 
 import logo from '../../../../static/logo.png'
-// probando vistas
-import MentoringPage from 'routes/Mentoring/components/MentoringPage/MentoringPage';
-import MenteePage from 'routes/Mentoring/components/MentoringPage/menteePage/MenteePage';
-import MentorPage from 'routes/Mentoring/components/MentoringPage/mentorPage/MentorPage';
-import FinancingPage from 'routes/Financing/components/FinancingPage/FinancingPage';
 
 function Home() {
   return (
     <section className="d-flex justify-content-center flex-column m-4">
       <div className="d-flex justify-content-center flex-column mt-2 mb-3">
         <figure className="d-flex justify-content-center rounded-circle">
-          <img src={logo} className="img-fluid maxh-40vh rounded-circle" alt="women"></img>
+          <img src={logo} className="img-fluid logo rounded-circle" alt="women"></img>
         </figure>
         <p className="text-center">It is a platform that allows women entrepreneurs (SMEs) 
         to connect with mentors from different fields to enhance their technical and soft skills,
