61f55cedb0962490395231cf546d4307e6d4124b
Danielalab <olabolacrayola@hotmail.com>
2019-05-15 17:42:37 -0500

    adding fintechs list view

--- src/routes/Financing/components/Fintech-list/FintechList.js
+++ src/routes/Financing/components/Fintech-list/FintechList.js
@@ -4,17 +4,20 @@ import img from '../../../../static/money.png'
 function FintechsList() {
   return(
     <section className="d-flex justify-content-center flex-column m-4">
-      <div className="d-flex justify-content-center flex-column mt-2 mb-3">
-        <figure className="d-flex justify-content-center rounded-circle">
-          <img src={img} className="img-fluid maxh-40vh rounded-circle" alt="women"></img>
-        </figure>
-        <h2 className="text-center mb-2">
-          <span className="title-logo">FinTee</span>
-          <span className="title title-green"> + Fintechs</span>
-        </h2>
-        <p className="text-center">FinTee allows you to connect women entrepreneurs with 
-        fintech from the region so they can access loans online.</p>
-      </div>
+      <p className="text-center">
+        Welcome, we will show you different fintechs in your country so you 
+        can choose the one that best suits your needs.
+      </p>
+      <form className="form" name="formLogin" role="form">
+        <div className="form-group">
+          <select id="country" className="form-control">
+            <option selected>Select your country</option>
+          </select>
+        </div>
+      </form>
+      <ul>
+        <li>lista de fintechs</li>
+      </ul>
     </section>
   )
 }
