debc0b64d19679e4ae31ea77a251e786c65f7d5a
Danielalab <olabolacrayola@hotmail.com>
2019-05-15 17:29:22 -0500

    change financing view

--- src/routes/Financing/components/FinancingPage/FinancingPage.js
+++ src/routes/Financing/components/FinancingPage/FinancingPage.js
@@ -6,14 +6,20 @@ function FinancingPage() {
     <section className="d-flex justify-content-center flex-column m-4">
       <div className="d-flex justify-content-center flex-column mt-2 mb-3">
         <figure className="d-flex justify-content-center rounded-circle">
-          <img src={img} className="img-fluid maxh-40vh rounded-circle" alt="women"></img>
+          <img src={img} className="img-fluid maxh-40vh rounded-circle" alt="money"></img>
         </figure>
         <h2 className="text-center mb-2">
           <span className="title-logo">FinTee</span>
-          <span className="title title-green"> + Fintechs</span>
+          <span className="title title-yellow"> + Fintechs</span>
         </h2>
-        <p className="text-center">FinTee allows you to connect women entrepreneurs with 
-        fintech from the region so they can access loans online.</p>
+        <p className="text-center">
+          FinTee allows women entrepreneurs to find more affordable 
+          financing options and online through lending fintechs.
+        </p>
+      </div>
+      <div className="d-flex justify-content-center flex-column">
+        <button type="button" className="btn btn-success btn-lg btn-block">I'm a women entrepreneur</button>
+        <button type="button" className="btn btn-outline-success btn-lg btn-block">I'm a Fintech</button>
       </div>
     </section>
   )
---
+++ src/routes/Financing/components/Fintech-list/FintechList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import img from '../../../../static/money.png'
+
+function FintechsList() {
+  return(
+    <section className="d-flex justify-content-center flex-column m-4">
+      <div className="d-flex justify-content-center flex-column mt-2 mb-3">
+        <figure className="d-flex justify-content-center rounded-circle">
+          <img src={img} className="img-fluid maxh-40vh rounded-circle" alt="women"></img>
+        </figure>
+        <h2 className="text-center mb-2">
+          <span className="title-logo">FinTee</span>
+          <span className="title title-green"> + Fintechs</span>
+        </h2>
+        <p className="text-center">FinTee allows you to connect women entrepreneurs with 
+        fintech from the region so they can access loans online.</p>
+      </div>
+    </section>
+  )
+}
+
+export default FintechsList;
