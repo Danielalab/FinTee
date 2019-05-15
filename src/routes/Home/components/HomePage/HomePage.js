49fa6548dbf121b11d35ea903cbb17ebc241d83a
MaiaRojas <maia.rt.46@gmail.com>
2019-05-15 18:07:25 -0400

    change colors

--- src/routes/Home/components/HomePage/HomePage.js
+++ src/routes/Home/components/HomePage/HomePage.js
@@ -21,10 +21,12 @@ function Home() {
         <figure className="d-flex justify-content-center rounded-circle">
           <img src={logo} className="img-fluid maxh-40vh rounded-circle" alt="women"></img>
         </figure>
-        <p className="text-center">It is a platform that allows women entrepreneurs (SMEs) 
-        to connect with mentors from different fields to enhance their technical and soft skills,
-        in addition to connecting them with fintechs from the region to access credit in 
-        a 100% digital experience.</p>
+        <p className="text-center">
+          It is a platform that allows women entrepreneurs (SMEs)
+          to connect with mentors from different fields to enhance their technical and soft skills,
+          in addition to connecting them with fintechs from the region to access credit in
+          a 100% digital experience.
+        </p>
       </div>
       <div className="d-flex justify-content-center flex-column">
         <button type="button" className="btn btn-next btn-lg btn-block">Mentoring</button>
