4bcab481c1ac730786a3fefd988d4397eab9f83a
Danielalab <olabolacrayola@hotmail.com>
2019-05-15 16:24:28 -0500

    add home view

--- src/routes/Home/components/HomePage/HomePage.js
+++ src/routes/Home/components/HomePage/HomePage.js
@@ -16,11 +16,21 @@ import FinancingPage from 'routes/Financing/FinancingPage';
 
 function Home() {
   return (
-
-    <FinancingPage />
-    // <MentorPage />
-    // <MenteePage />
-    // <MentoringPage />
+    <section className="d-flex justify-content-center flex-column m-4">
+      <div className="d-flex justify-content-center flex-column mt-2 mb-3">
+        <figure className="d-flex justify-content-center rounded-circle">
+          <img src={logo} className="img-fluid maxh-40vh rounded-circle" alt="women"></img>
+        </figure>
+        <p className="text-center">It is a platform that allows women entrepreneurs (SMEs) 
+        to connect with mentors from different fields to enhance their technical and soft skills,
+        in addition to connecting them with fintechs from the region to access credit in 
+        a 100% digital experience.</p>
+      </div>
+      <div className="d-flex justify-content-center flex-column">
+        <button type="button" className="btn btn-next btn-lg btn-block">Mentoring</button>
+        <button type="button" className="btn btn-next btn-lg btn-block">Financing</button>
+      </div>
+    </section>
   )
 }
 
