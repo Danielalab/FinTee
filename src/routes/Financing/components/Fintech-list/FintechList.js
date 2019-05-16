07d4bed48a795df202394346c87876086ab62dc3
Danielalab <olabolacrayola@hotmail.com>
2019-05-15 21:31:49 -0500

    adding fintechs list

--- src/routes/Financing/components/Fintech-list/FintechList.js
+++ src/routes/Financing/components/Fintech-list/FintechList.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import { countries } from 'countries-apec';
 
-function FintechsList() {
-  return(
-    <section className="d-flex justify-content-center flex-column m-4">
-      <p className="text-center">
-        Welcome, we will show you different fintechs in your country so you 
-        can choose the one that best suits your needs.
-      </p>
-      <form className="form" name="formLogin" role="form">
-        <div className="form-group">
-          <select id="country" className="form-control">
-            <option selected>Select your country</option>
-            { countries.map(country => <option key={country.id}>{ country.name }</option>) }
-          </select>
-        </div>
-      </form>
-      <ul>
-        <li>lista de fintechs</li>
-      </ul>
-    </section>
-  )
+class FintechsList extends React.Component() {
+  state = {
+    error: null,
+    data: undefined,
+  };
+
+  fetchData = async (countryName) => {
+    this.setState({ data: null, error: null });
+
+    try {
+      let data = await fetch('../../../');
+      data = data.filter(fintech => fintech.country === countryName)
+      this.setState({ data: data });
+    } catch (error) {
+      this.setState({ error: error });
+    }
+  };
+
+  handleSelectCountry = (country) => {
+    this.fetchData(country);
+  }
+
+  render() {
+    return(
+      <section className="d-flex justify-content-center flex-column m-4">
+        <p className="text-center">
+          Welcome, we will show you different fintechs in your country so you 
+          can choose the one that best suits your needs.
+        </p>
+        <form className="form" name="formLogin" role="form">
+          <div className="form-group">
+            <select onChange={(e) => { handleSelectCountry(e.target.value) }} id="country" className="form-control">
+              <option selected >Select your country</option>
+              { countries.map(country => <option key={country.id}  value={country.name}>{ country.name }</option>) }
+            </select>
+          </div>
+        </form>
+        { data ? 'hola' : '' }
+        <ul>
+          <li>lista de fintechs</li>
+        </ul>
+      </section>
+    )
+  }
 }
 
 export default FintechsList;
