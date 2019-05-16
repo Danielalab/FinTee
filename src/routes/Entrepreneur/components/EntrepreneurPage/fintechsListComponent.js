6eb28c40ceba97b1006f273abff4dfa3517bfe69
Danielalab <olabolacrayola@hotmail.com>
2019-05-16 01:58:00 -0500

    fixed error

---
+++ src/routes/Entrepreneur/components/EntrepreneurPage/fintechsListComponent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { countries } from 'countries-apec';
+import { fintechsList } from 'routes/Financing/fintechs';
+
+function FintechItem(fintechData) {
+  return(
+    <div className="card">
+      <img src={ fintechData.img } className="card-img-top" alt={ fintechData.nameFintech } />
+      <div className="card-body">
+        <h5 className="card-title">{ fintechData.nameFintech }</h5>
+        <a href={ fintechData.webURL } className="btn btn-success">Ask for a loan</a>
+      </div>
+    </div>
+  )
+}
+
+class FintechsListComponent extends React.Component {
+  state = {
+    data: []
+  }
+  handleSelectCountry = (countryName) => {
+    this.setState({ data: [] })
+    const dataCountry = fintechsList.filter((fintech) => fintech.country === countryName)
+    this.setState({ data: dataCountry })
+  }
+  render() {
+    return(
+      <div>
+        <form className="form" name="formLogin" role="form">
+          <div className="form-group">
+            <select onChange={(e) => { this.handleSelectCountry(e.target.value) }} id="country" className="form-control">
+              <option selected value="select">Select your country</option>
+              { countries.map(country => <option key={country.id}  value={country.name}>{ country.name }</option>) }
+            </select>
+          </div>
+        </form>
+        <ul>
+          { this.state.data.map(fintech => {
+            return (
+              <li key={ fintech.id }>
+                {FintechItem(fintech)}
+              </li>
+            )
+          }) }
+        </ul>  
+      </div>
+    )
+  }
+}
+
+export default FintechsListComponent;
